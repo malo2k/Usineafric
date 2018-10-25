98abd36e9bcaeb6494d062364dde39257a39d820
malo2k <malo2k@laposte.net>
2018-10-25 23:03:49 +0200

    last push

--- db/migrate/20181025143059_create_assemblies.rb
+++ db/migrate/20181025143059_create_assemblies.rb
@@ -1,6 +1,7 @@
 class CreateAssemblies < ActiveRecord::Migration[5.2]
   def change
     create_table :assemblies do |t|
+    	t.string :name
 
       t.timestamps
     end
--- db/migrate/20181025143250_create_parts.rb
+++ db/migrate/20181025143250_create_parts.rb
@@ -1,6 +1,7 @@
 class CreateParts < ActiveRecord::Migration[5.2]
   def change
     create_table :parts do |t|
+    	t.string :part_number
 
       t.timestamps
     end
